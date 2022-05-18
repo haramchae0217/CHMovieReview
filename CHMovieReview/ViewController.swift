b8a1f836e67ae1ff58b27eaeeae9683a3805ac24
Chae Haram <djvemfla2@gmail.com>
2022-05-18 23:02:21 +0900

    create project

---
+++ CHMovieReview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CHMovieReview
+//
+//  Created by Chae_Haram on 2022/05/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
